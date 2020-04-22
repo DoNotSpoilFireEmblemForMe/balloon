e4b8998c10364182af52db6f4612d569b8f195a5
Elvis Ng <eng18@ucsc.edu>
2020-04-21 21:32:11 -0700

    update

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -17,10 +17,10 @@ class Play extends Phaser.Scene {
 
   create() {
       // place tile sprite
+      this.cloudfront = this.add.tileSprite(0, 0, 640, 480, 'cloudback').setOrigin(0, 0);
+      this.cloudback = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
       this.starfield = this.add.tileSprite(0, 0, 640, 480, 'cloudfront').setOrigin(0, 0);
-      this.starfield = this.add.tileSprite(0, 0, 640, 480, 'cloudback').setOrigin(0, 0);
-      this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
-
+      
       // white rectangle borders
       this.add.rectangle(5, 5, 630, 32, 0xFFFFFF).setOrigin(0, 0);
       this.add.rectangle(5, 443, 630, 32, 0xFFFFFF).setOrigin(0, 0);
@@ -28,7 +28,7 @@ class Play extends Phaser.Scene {
       this.add.rectangle(603, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
 
       // green UI background
-      this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0, 0);
+      //this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0, 0);
 
       // add rocket (p1)
       this.p1Rocket = new Rocket(this, game.config.width/2 - 8, 431, 'rocket').setScale(0.5, 0.5).setOrigin(0, 0);
