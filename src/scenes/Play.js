0cf2a6c11bb06e9a897f2917d02f21558cef8623
Elvis Ng <eng18@ucsc.edu>
2020-04-21 19:15:29 -0700

    update

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -26,8 +26,9 @@ class Play extends Phaser.Scene {
       this.add.rectangle(5, 443, 630, 32, 0xFFFFFF).setOrigin(0, 0);
       this.add.rectangle(5, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
       this.add.rectangle(603, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
+
       // green UI background
-      //this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0, 0);
+      this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0, 0);
 
       // add rocket (p1)
       this.p1Rocket = new Rocket(this, game.config.width/2 - 8, 431, 'rocket').setScale(0.5, 0.5).setOrigin(0, 0);
@@ -87,6 +88,7 @@ class Play extends Phaser.Scene {
           this.scene.start("menuScene");
       }
 
+      this.cloudfront.tilePositionX -= 4;
       this.cloudback.tilePositionX -= 2;  // scroll tile sprite
 
       if (!this.gameOver) {               
